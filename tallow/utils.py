630b41f01b1d2cce3e430cd9141641af711fdfa4
Charlton Lim <cadmusthefounder@gmail.com>
2019-03-12 18:50:37 +0800

    refactor

--- tallow/utils.py
+++ tallow/utils.py
@@ -130,7 +130,7 @@ def encode_frequency(frequency_map, categorical_or_mvc_data):
     result = np.array([])
     for i in range(categorical_or_mvc_data.shape[1]):
         encoded_col = np.array(list(map(lambda x: dict(frequency_map[i])[x], categorical_or_mvc_data[:,i])))
-        result = encoded_col if i == 0 else np.concatenate((result, encoded_col), axis=1)
+        result = [encoded_col] if i == 0 else np.concatenate((result, encoded_col), axis=1)
     print('File: {} Class: {} Function: {} State: {} \n'.format('utils.py', 'None', 'encode_frequency', 'End'))
     return result
 
