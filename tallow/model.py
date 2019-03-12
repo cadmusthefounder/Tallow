9731c5f082592e518a3392314ac5a3ca74298dbb
Charlton Lim <cadmusthefounder@gmail.com>
2019-03-12 21:10:47 +0800

    refactor

--- tallow/model.py
+++ tallow/model.py
@@ -10,51 +10,12 @@ architecture_mapping = {
     CATBOOST_ENSEMBLE.NAME: CATBOOST_ENSEMBLE
 }
 
-import numpy as np
-import pandas as pd
-from collections import Counter
-from utils import encode_frequency
-
 class Model:
 
     def __init__(self, datainfo, timeinfo):
         self._architecture = architecture_mapping[ARCHITECTURE](datainfo, timeinfo)
         
     def fit(self, F, y, datainfo, timeinfo):
-        count = {}
-        X1 = [
-            ['apple', '5', 'dog'],
-            ['orange', '2', 'cat'],
-            ['pear', '3', 'dog'],
-            ['pear', '1', 'dog'],
-            ['apple', '1', 'cat'],
-            ['apple', '2', 'cat'],
-            ['apple', '3', 'dog'],
-            ['pear', '4', 'dog']
-        ]
-        X1 = np.array(X1)
-        for i in range(X1.shape[1]):
-            count[i] = Counter(X1[:,i])
-
-        print(count)
-        X2 = [
-            ['apple', '1', 'cat'],
-            ['orange', '2', 'cat'],
-            ['pear', '5', 'dog'],
-            ['pear', '5', 'cat'],
-            ['orange', '1', 'cat'],
-            ['orange', '5', 'cat'],
-            ['orange', '3', 'dog'],
-            ['grapes', '2', 'dog']
-        ]
-        X2 = np.array(X2)
-        for i in range(X2.shape[1]):
-
-            count[i] += Counter(X2[:,i])
-        print(count)
-
-        res = encode_frequency(count, X1)
-        print(res)
         self._architecture.fit(F, y, datainfo, timeinfo)
 
     def predict(self, F, datainfo, timeinfo):
