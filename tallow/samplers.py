aa62af5e6ca18debf97b3e9b0366d1bb087f0aa8
Charlton Lim <cadmusthefounder@gmail.com>
2019-03-13 23:07:05 +0800

    Add stuff

--- tallow/samplers.py
+++ tallow/samplers.py
@@ -81,7 +81,7 @@ class RandomUnderSampler:
         return dict(zip(unique, counts))
 
     def _sampling_strategy_not_minority(self, y):
-        target_stats = _count_class_sample(y)
+        target_stats = self._count_class_sample(y)
         n_sample_minority = min(target_stats.values())
         class_minority = min(target_stats, key=target_stats.get)
         sampling_strategy = {
