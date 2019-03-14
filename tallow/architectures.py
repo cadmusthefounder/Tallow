fa111e23b9c3befca0d6a3c55bb55c64ee45e60e
Charlton Lim <cadmusthefounder@gmail.com>
2019-03-14 23:22:17 +0800

    Add stuff

--- tallow/architectures.py
+++ tallow/architectures.py
@@ -149,8 +149,8 @@ class OriginalEnsemble:
                 self._epsilon
             )
 
-            validation_train_data, validation_train_labels = self._imbalanced_sampler.sample(validation_data, validation_labels, free_raw_data=False)
-            validation_train_dataset = lgbm.Dataset(validation_train_data, validation_train_labels)
+            validation_train_data, validation_train_labels = self._imbalanced_sampler.sample(validation_data, validation_labels)
+            validation_train_dataset = lgbm.Dataset(validation_train_data, validation_train_labels, free_raw_data=False)
             new_classifier = lgbm.train(
                 params=self._best_hyperparameters_clone, 
                 train_set=validation_train_dataset, 
